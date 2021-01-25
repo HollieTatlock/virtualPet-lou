bae99d0dbd37aefc6f39b7403cfe6ec818677c41
Hollie Tatlock <hollietatlock@gmail.com>
2021-01-25 20:42:32 +0000

    updated

--- src/pet.js
+++ src/pet.js
@@ -1,12 +1,22 @@
+const MAXIMUM_FITNESS = 10;
+
+//Hunger
+const MINIMUM_HUNGER = 0;
+
+
+
 function Pet(name) {
 this.name = name; 
 this.age = 0;
 this.hunger = 0;
-this.fitness = 10;
+this.fitness = MAXIMUM_FITNESS;
 
 };
 
 Pet.prototype = {
+
+  
+
 growUp() {
     this.age ++;    // this increases the age by 1 when growUp is passed
     this.hunger  += 5;  // this increases the hunger by 5 when growUp is passed
@@ -25,13 +35,43 @@ walk() {
         }
         else if ((this.fitness === 9)) {
             this.fitness += 1;
-            
         }
-        else if ((this.fitness === 10)) {
+        else if ((this.fitness === MAXIMUM_FITNESS )) {
             return "Maximum fitness reached";
             
         }
+    // need to add less than 0 or 0 itself then dead 
+    },
+    feed() {
+        if ((this.hunger >=3 )) {
+            this.hunger -=3;
+        }
+        else if ((this.hunger === 2)) {
+            this.hunger -= 2;
+        }
+        else if ((this.hunger ===1)) {
+            this.hunger -= 1;
+        }
     
+        if (this.hunger <= MINIMUM_HUNGER) {
+            throw "I'm not hungry"
+        }
+
+    },
+    checkUp() {
+   
+        if ((this.hunger >= 5 && this.fitness<= 3)) {
+            return "I am hungry and I need a Walk!";
+    
+        } 
+        else if ((this.hunger >= 5 && this.fitness>= 3)) {
+            return "I am hungry!";
+    
+        } else if ((this.fitness<= 3 && this.hunger <= 5)) {
+            return "I need a walk!";
+    }
+    else
+        return "I feel great!";
     }
 }
 module.exports = {Pet};
--- tests/pet.test.js
+++ tests/pet.test.js
@@ -35,16 +35,51 @@ describe("Grow up get hungier and less fitter", () => {
   expect(pet.age).toEqual(1);
   expect(pet.hunger).toEqual(5);
   expect(pet.fitness).toEqual(7);
-});
+  });
 });
 
 describe("Walking your pet", () => {
   it("increases its fitness levels by 4 when 'walk' is passed but no more than 10", () => {
   const pet = new Pet("Louie");
-  pet.fitness = 7;
+  pet.fitness = 3;
   pet.walk();
-
-  expect(pet.fitness).toEqual(10);
+  expect(pet.fitness).toEqual(7);
+  });
 });
-});
 
+describe("Feeding your pet", () => {
+  it("decreases your pets hunger by 3 when 'feed' is passed but hunger should not fall below 0", () => {
+    const pet = new Pet("Louie");
+    pet.hunger = 7
+    pet.feed();
+    expect(pet.hunger).toEqual(4);
+  });
+});
+ describe('checkUp', () => {
+
+  it('Lets you know if the pet is hungry >= 5', () => {
+      const pet = new Pet("Louie");
+        pet.hunger = 7;
+        expect(pet.checkUp()).toBe("I am hungry!");
+    });
+    
+  it('Lets you know if the pet needs a walk <= 3', () => {
+      const pet = new Pet("Louie");
+        pet.fitness = 1
+        expect(pet.checkUp()).toBe("I need a walk!");
+    });
+
+  it('lets you know if the pet is hungry and needs a walk if hunger is >= 5 & fitness is <= 3', () => {
+    const pet = new Pet("Louie");
+    pet.hunger = 6;
+    pet.fitness = 2;
+    expect(pet.checkUp()).toBe("I am hungry and I need a Walk!");
+  });
+
+  it('Says pet feels great if hunger is <5 and fitness is >3', () => {
+    const pet = new Pet("Louie");
+    pet.hunger = 2
+    pet.fitness = 10
+    expect(pet.checkUp()).toBe("I feel great!");
+  });
+});
